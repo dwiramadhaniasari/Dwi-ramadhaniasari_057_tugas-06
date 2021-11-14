3f508ff72f2870ca2fb6058383ba829ad9bf69a6
dwiramadhaniasari <90232642+dwiramadhaniasari@users.noreply.github.com>
2021-11-14 07:25:30 +0700

    Add files via upload

---
+++ program 23.cpp
@@ -0,0 +1,20 @@
+/* Program Duapuluh empat */
+/* Program sederhana menggunakan fungsi */
+/* yang tidak mengembalikan nilai */ 
+
+#include<stdio.h>
+#include<conio.h>
+
+void garis();
+main()
+{
+	garis();
+	printf("NO NIM NAMA  NILAI \n");
+	garis();
+	getch();
+}
+void garis ()               /* definisi fungsi */
+{
+	printf("======================== \n");
+}
+
